881fe265d885de5b822923a8bbba858e7a1d88dc
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-05 14:48:22 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import IndexScreen from "./src/screens/IndexScreen";
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
+  },
+  {
+    // config options
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blog"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
