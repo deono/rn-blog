55ed3d45a8746b509b11e78c624ff6bebab94cbe
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-05 14:54:32 +0100

    wrapping the navigator

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
@@ -16,4 +18,8 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
