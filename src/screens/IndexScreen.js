4bf49bbc1496925c7c36fa52b2ab10bd23327c2d
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-06 13:12:48 +0100

    blog index styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blog => blog.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.rowStyles}>
+              <Text style={styles.titleStyles}>{item.title}</Text>
+              <Feather style={styles.iconStyles} name="trash-2" />
+            </View>
+          );
         }}
       />
     </View>
@@ -21,6 +26,20 @@ const IndexScreen = () => {
 
 const styles = StyleSheet.create({
   // styles
+  rowStyles: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray"
+  },
+  titleStyles: {
+    fontSize: 18
+  },
+  iconStyles: {
+    fontSize: 24
+  }
 });
 
 export default IndexScreen;
