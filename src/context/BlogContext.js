edb0d95b1ca30f91727599f32fd6b5bc84ef93d4
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-06 08:25:06 +0100

    replace useState with useReducer to manage application state

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_BLOGPOST":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` }
-    ]);
+    dispatch({ type: "ADD_BLOGPOST" });
   };
 
   return (
