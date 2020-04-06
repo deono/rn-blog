d638022ed5026b66eb1b6e2199fcc18c8b51245c
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-06 13:54:27 +0100

    show a blogpost in ShowScreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const ShowScreen = () => {
+  const { state } = useContext(Context);
+
+  // get the post to show from the state
+  const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam("id")
+  );
+
   return (
     <View>
-      <Text>Show Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 };
