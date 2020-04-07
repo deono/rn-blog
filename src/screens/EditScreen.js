f19d8315758699cedbced2093b7d9cf9288456bc
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-07 13:33:39 +0100

    initializing state from context in EditScreen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,16 +1,47 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useContext } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam("id")
+  );
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
-    <View>
-      <Text>Edit Screen - {navigation.getParam("id")}</Text>
+    <View style={styles.container}>
+      <Text style={styles.labelStyles}>Edit Title</Text>
+      <TextInput
+        style={styles.inputStyles}
+        value={title}
+        onChangeText={newTitle => setTitle(newTitle)}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   //styles
+  container: {
+    margin: 10
+  },
+  inputStyles: {
+    fontSize: 18,
+    backgroundColor: "#C4C4C4",
+    color: "white",
+    padding: 5,
+    borderRadius: 5,
+    marginBottom: 10
+  },
+  labelStyles: {
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 5
+  }
 });
 
 export default EditScreen;
